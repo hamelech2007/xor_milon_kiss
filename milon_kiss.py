7e53d103f8aff13d22a656437ed9996b1c6da33f
hamelech2007 <tomerstemer2007@gmail.com>
2025-11-04 10:25:38 +0200

    Fixed dictionary naming, display menu function, unknown option and race conditions

--- milon_kiss.py
+++ milon_kiss.py
@@ -7,64 +7,66 @@ DELETE_WORD = "3"
 EXIT = "4"
 
 
-def display_menu() -> None:
-    """Displays the choice menu."""
-    menu = {
+def display_menu(menu: Dict[str, str]) -> None:
+    """Displays a given menu."""
+    for key, val in menu.items():
+        print(f"{key}. {val}")
+
+
+def handle_get_word(words_dict: Dict[str, str]) -> None:
+    """Gets a translation from the dictionary"""
+    word_to_get = input("Word to get: ")
+    word_meaning = words_dict.get(word_to_get)
+    if word_meaning is None:
+        print(f"The word {word_to_get} doesnt exist!")
+        return
+    print(f"{word_to_get} means {word_meaning}.")
+
+
+def handle_add_word(words_dict: Dict[str, str]) -> None:
+    """Adds a word to the dictionary"""
+    word_to_add = input("Word to add: ")
+    word_meaning = input(f"Meaning of {word_to_add}: ")
+    words_dict[word_to_add] = word_meaning
+    print(f"{word_to_add}={word_meaning} was added.")
+
+
+def handle_delete_word(words_dict: Dict[str, str]) -> None:
+    """Deletes a word from the dictionary"""
+    word_to_delete = input("Word to delete: ")
+    deleted_word = words_dict.pop(word_to_delete, None)
+    if deleted_word is None:
+        print("Word does not exist!")
+        return
+    print(f"{word_to_delete} has been erased.")
+
+
+def main() -> None:
+    main_menu = {
         GET_WORD: "Get word",
         ADD_WORD: "Add word",
         DELETE_WORD: "Delete word",
         EXIT: "Exit"
     }
-
-    for key, val in menu.items():
-        print(f"{key}. {val}")
-
-
-def handle_get_word(dic: Dict[str, str]) -> None:
-    """Gets a translation from the dictionary"""
-    word_to_get = input("Word to get: ")
-    if word_to_get not in dic:
-        print(f"The word {word_to_get} doesnt exist!")
-        return
-    print(f"{word_to_get} means {dic[word_to_get]}.")
-
-
-def handle_add_word(dic: Dict[str, str]) -> None:
-    """Adds a word to the dictionary"""
-    word_to_add = input("Word to add: ")
-    word_meaning = input(f"Meaning of {word_to_add}: ")
-    dic[word_to_add] = word_meaning
-    print(f"{word_to_add}={word_meaning} was added.")
-
-
-def handle_delete_word(dic: Dict[str, str]) -> None:
-    """Deletes a word from the dictionary"""
-    word_to_delete = input("Word to delete: ")
-    if word_to_delete not in dic:
-        print("Word does not exist!")
-        return
-    del dic[word_to_delete]
-    print(f"{word_to_delete} has been erased.")
-
-
-def main() -> None:
-    dic: Dict[str, str] = {}
+    words_dict: Dict[str, str] = {}
     while True:
-        display_menu()
+        display_menu(main_menu)
         choice = input("Enter choice: ")
 
         if choice == GET_WORD:
-            handle_get_word(dic)
+            handle_get_word(words_dict)
 
         elif choice == ADD_WORD:
-            handle_add_word(dic)
+            handle_add_word(words_dict)
 
         elif choice == DELETE_WORD:
-            handle_delete_word(dic)
+            handle_delete_word(words_dict)
 
         elif choice == EXIT:
             print("Goodbye!")
             break
+        else:
+            print("Invalid choice!")
 
 
 if __name__ == "__main__":
